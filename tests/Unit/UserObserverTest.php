fdd78a57e1260afba68a55b0e1fec0c37e7a3155
Nasrul Hazim <nasrulhazim.m@gmail.com>
2019-02-23 12:06:06 +0800

    Added it can create hashslug test

--- tests/Unit/UserObserverTest.php
+++ tests/Unit/UserObserverTest.php
@@ -8,10 +8,20 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class UserObserverTest extends TestCase
 {
+	use RefreshDatabase;
+
     /** @test */
     public function it_has_user_observer_class()
     {
     	$this->assertTrue(class_exists('\App\Observers\UserObserver'));
     }
-    // add more test..ensure observer is register, it works as expected
+
+    /** @test */
+    public function it_can_create_hashslug()
+    {
+    	$user = factory(\App\User::class)->create();
+
+    	// assert user->hashslug is not empty
+    	$this->assertNotNull($user->hashslug);
+    }
 }
