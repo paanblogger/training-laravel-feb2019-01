d8b8a3f2e0d0786061de1556d60069e0065b9d90
Nasrul Hazim <nasrulhazim.m@gmail.com>
2019-02-24 11:49:53 +0800

    Added Auth Test

---
+++ tests/Feature/AuthTest.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class AuthTest extends TestCase
+{
+    use RefreshDatabase;
+
+    protected function setUp()
+    {
+        parent::setUp();
+        // $user = factory(\App\User::class)->create([
+        //     'name' => 'PHPUnit Test',
+        //     'email' => 'test@unit.php',
+        //     'password' => \Illuminate\Support\Facades\Hash::make('password'),
+        // ]);
+    }
+
+    /** @test */
+    public function it_can_get_status_code_200_for_login_page()
+    {
+        $this->get('/login')->assertStatus(200);
+    }
+
+    /** @test */
+    public function it_can_get_see_see_form_inputs()
+    {
+        $this->get('/login')
+            ->assertSee('E-Mail Address')
+            ->assertSee('Password')
+            ->assertSee('Login');
+    }
+
+    /** @test */
+    public function it_can_login()
+    {
+        $user = factory(\App\User::class)->create([
+            'email' => 'test@unit.php',
+            'password' => \Illuminate\Support\Facades\Hash::make('password'),
+        ]);
+
+        $this->post('/login', [
+            'email' => 'test@unit.php', 'password' => 'password'
+        ])->assertStatus(302);
+    }
+
+    /** @test */
+    public function it_can_register()
+    {
+        $this->post('/login', [
+            'name' => 'PHPUnit Test',
+            'email' => 'test@unit.php', 
+            'password' => 'password',
+            'password_confirmation' => 'password',
+        ])->assertStatus(302);
+    }
+}
