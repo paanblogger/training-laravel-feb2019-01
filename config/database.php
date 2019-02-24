34bc8efdb86d6edcd0872adeadb90dba68a30165
Nasrul Hazim <nasrulhazim.m@gmail.com>
2019-02-24 13:47:18 +0800

    Added Dusk Test on Registration User

--- config/database.php
+++ config/database.php
@@ -40,6 +40,13 @@ return [
             'foreign_key_constraints' => env('DB_FOREIGN_KEYS', true),
         ],
 
+        'dusk' => [
+            'driver' => 'sqlite',
+            'database' => env('DB_DATABASE', database_path('dusk.sqlite')),
+            'prefix' => '',
+            'foreign_key_constraints' => env('DB_FOREIGN_KEYS', false),
+        ],
+
         'mysql' => [
             'driver' => 'mysql',
             'host' => env('DB_HOST', '127.0.0.1'),
--- tests/Browser/LandingPageTest.php
+++ tests/Browser/LandingPageTest.php
@@ -8,15 +8,40 @@ use Illuminate\Foundation\Testing\DatabaseMigrations;
 
 class LandingPageTest extends DuskTestCase
 {
+    use DatabaseMigrations;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+    }
+
     /** @test */
     public function it_has_proper_landing_page()
     {
         $this->browse(function (Browser $browser) {
             $browser->visit('/')
-                    ->pause(self::PAUSE_DURATION)
                     ->assertSee('Laravel')
                     ->assertSeeLink('Login')
-                    ->assertSeeLink('Register');
+                    ->assertSeeLink('Register')
+                    ->pause(self::PAUSE_DURATION)
+                    ->clickLink('Login')
+                    ->assertPathIs('/login')
+                    ->assertSeeLink('Login')
+                    ->assertSeeLink('Register')
+                    ->pause(self::PAUSE_DURATION)
+                    ->clickLink('Register')
+                    ->assertPathIs('/register')
+                    ->assertSeeLink('Login')
+                    ->assertSeeLink('Register')
+                    ->pause(self::PAUSE_DURATION)
+                    ->type('name', 'Laravel Dusk')
+                    ->type('email', 'dusk@laravel.com')
+                    ->pause(self::PAUSE_DURATION)
+                    ->type('password', 'password')
+                    ->type('password_confirmation', 'password')
+                    ->press('Register')
+                    ->assertPathIs('/home')
+                    ->pause(self::PAUSE_DURATION);
         });
     }
 }
